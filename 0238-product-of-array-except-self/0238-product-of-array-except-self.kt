dd51999d15d8357664822741bc192a4ca3cbdd25
Sanjeet Yadav <87947328+dukendev@users.noreply.github.com>
2023-09-19 10:37:46 -0700

    Time: 274 ms (95.04%), Space: 47.5 MB (93.5%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.kt
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.kt
@@ -1,23 +1,24 @@
 class Solution {
     fun productExceptSelf(nums: IntArray): IntArray {
         val result = IntArray(nums.size)
-        //build prefix 
-        val prefix = IntArray(nums.size)
-        prefix[0] = nums[0]
-        for(i in 1..nums.size-1){
-            prefix[i] = prefix[i-1].times(nums[i])
-        }
-        //build postfix
-        val postfix = IntArray(nums.size)
-        postfix[nums.size -1] = 1
+
+        var post = 1
+        result[nums.size - 1] = 1
         for(j in nums.size - 2 downTo 0){
-            postfix[j] = postfix[j+1].times(nums[j+1])
+            result[j] = post * nums[j+1]
+            post = result[j]
         }
-        //set result
-        result[0] = postfix[0]
-        for(i in 1..result.size-1){
-            result[i] = prefix[i-1].times(postfix[i])
+
+        var pre = 1
+        for(i in 0..nums.size-1){
+            result[i] *= pre
+            pre *= nums[i]
         }
+  
+
+        
+ 
+
         
         return result
         
