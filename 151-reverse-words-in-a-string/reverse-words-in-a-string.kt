eeaae4538ca27070b873b69ba40230995ddc6aa1
Sanjeet Yadav <87947328+dukendev@users.noreply.github.com>
2023-12-01 00:07:47 +0530

    Time: 223 ms (25.20%) | Memory: 37.9 MB (60.45%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.kt
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.kt
@@ -1,7 +1,11 @@
 class Solution {
     fun reverseWords(s: String): String {
-        return s.split(" ").filter {
+        return s.split(" ")
+        .filter {
             it.isNotEmpty()
-        }.reversed().joinToString(" ").trim()
+        }
+        .reversed()
+        .joinToString(" ")
+        .trim()
     }
 }
