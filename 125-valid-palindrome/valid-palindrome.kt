ccbf61515f05386cef9f0e85fdfe27368de4648a
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2024-05-18 15:53:05 +0530

    Time: 212 ms (36.23%) | Memory: 38.8 MB (22.60%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.kt
+++ 125-valid-palindrome/valid-palindrome.kt
@@ -1,18 +1,8 @@
 class Solution {
-fun isPalindrome(s: String): Boolean {
-        val filtered = s.filter {
-            it.isLetterOrDigit()
-        }.toLowerCase()
-
-        var i = 0
-        var j = filtered.length - 1
-        while(i < j){
-            if(filtered[i] != filtered[j]){
-                return false
-            }
-            i++
-            j--
-        }
-        return true
+    fun isPalindrome(s: String): Boolean {
+    val filtered = s.filter { it.isLetterOrDigit() }.toLowerCase()
+    return filtered
+        .zip(filtered.reversed())
+        .all { it.first == it.second }
     }
 }
