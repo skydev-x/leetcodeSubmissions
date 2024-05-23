461e94f5494378b913b1fe3802a6c46c295903cb
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2024-05-24 02:41:28 +0530

    Time: 684 ms (55.22%) | Memory: 61.8 MB (11.94%) - LeetSync

--- 883-car-fleet/car-fleet.kt
+++ 883-car-fleet/car-fleet.kt
@@ -2,18 +2,18 @@ class Solution {
 
     fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
         val cars = position.zip(speed).sortedByDescending { it.first }
-        val stk = Stack<Double>()
+        val stk = ArrayDeque<Double>()
 
         for ((pos, spd) in cars) {
             val time = (target - pos).toDouble() / spd
-            if (stk.isNotEmpty() && time <= stk.peek()) {
+            if (stk.isNotEmpty() && time <= stk.last()) {
                 continue
             } else {
-                stk.push(time)
+                stk.addLast(time)
             }
         }
 
         return stk.size
     }
-    
+
 }
