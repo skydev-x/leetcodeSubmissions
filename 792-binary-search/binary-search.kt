a6ed1987931a8e4e514dc92aaf61c68c4dafb8c3
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2024-05-26 02:33:21 +0530

    Time: 257 ms (24.91%) | Memory: 38.4 MB (96.74%) - LeetSync

--- 792-binary-search/binary-search.kt
+++ 792-binary-search/binary-search.kt
@@ -3,24 +3,16 @@ class Solution {
         if(nums.size == 1){
             return if(nums[0] == target) 0 else -1
         }
-        var l = 0
-        var r = nums.size - 1
-        return binarySearch(nums,l,r,target)
+        return binarySearch(nums,0,nums.size - 1,target)
     }
 
     fun binarySearch(nums : IntArray ,l : Int, r : Int, t : Int) : Int {
-        if(l > r){
-            return - 1
-        }
+        if(l > r) return - 1 
         val mid = (r - l) / 2 + l 
         return when {
-            nums[mid] < t -> {
-                binarySearch(nums,mid + 1,r,t)
-            }
-            nums[mid] > t -> {
-                binarySearch(nums,l,mid - 1,t)
-            }
-            else -> return mid
+            nums[mid] < t -> binarySearch(nums,mid + 1,r,t)
+            nums[mid] > t -> binarySearch(nums,l,mid - 1,t)
+            else -> mid
         }
     }
 }
