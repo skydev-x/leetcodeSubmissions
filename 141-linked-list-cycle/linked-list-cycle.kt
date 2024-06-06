55407629a57c010e6f88ac4153eaecc1d27d79d7
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2024-06-07 01:53:13 +0530

    Time: 206 ms (25.90%) | Memory: 37.7 MB (47.96%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.kt
@@ -0,0 +1,27 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        if(head == null || head?.next == null){
+            return false
+        }
+        var slow = head
+        var fast = head.next
+        while(fast != null && fast.next != null){
+            if(slow == fast){
+                return true
+            }
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+        return false
+    }
+}
