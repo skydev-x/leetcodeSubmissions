c5b2a5ab60287e35491860e1a5324edcb65bd793
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2024-06-11 01:05:51 +0530

    Time: 136 ms (68.48%) | Memory: 33.8 MB (47.81%) - LeetSync

---
+++ 100-same-tree/same-tree.kt
@@ -0,0 +1,23 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if(q == null && p != null) return false
+        if(p == null && q != null) return false
+        if(p == null && q == null) return true
+
+        return if(p?.`val` == q?.`val`){
+            isSameTree(q?.left,p?.left) && isSameTree(q?.right,p?.right)
+        } else false
+
+
+    }
+}
