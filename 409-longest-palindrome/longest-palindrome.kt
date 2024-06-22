c81e355de861dc048e830a6101049fbcbd7b0d8a
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2024-06-23 00:00:37 +0530

    Time: 160 ms (62.10%) | Memory: 34.3 MB (68.32%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.kt
+++ 409-longest-palindrome/longest-palindrome.kt
@@ -1,26 +1,28 @@
 class Solution {
     fun longestPalindrome(s: String): Int {
+
        val count = mutableMapOf<Char,Int>()
        for(c in s){
           count[c] = count.getOrPut(c){0} + 1
        }
-    var length = 0
-    var oddFound = false
+       var length = 0
+       var oddFound = false
     
-    for (value in count.values) {
-        if (value % 2 == 0) {
-            length += value
-        } else {
-            length += value - 1
-            oddFound = true
+        for (value in count.values) {
+            if (value % 2 == 0) {
+                length += value
+            } else {
+                length += value - 1
+                oddFound = true
+            }
         }
-    }
     
-    // Add one if there's any odd count to place in the center
-    if (oddFound) {
-        length += 1
-    }
+        if (oddFound) {
+            length += 1
+        }
     
-    return length
+        return length
+    
     }
+
 }
