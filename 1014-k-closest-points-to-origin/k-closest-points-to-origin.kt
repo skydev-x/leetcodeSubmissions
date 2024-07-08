e231c3f69c6770f857ecf8e02318c49d26ab3d7f
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2024-07-08 18:30:59 +0530

    Time: 668 ms (71.55%) | Memory: 63.4 MB (74.49%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.kt
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.kt
@@ -1,7 +1,20 @@
 class Solution {
     fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
-        return points.sortedBy { point ->
-            point[0] * point[0] + point[1] * point[1].toDouble()
-        }.take(k).toTypedArray()
+        var ans = Array(k) { intArrayOf() }
+        val pq = PriorityQueue<IntArray>(1, compareBy { getDistanceFromOrigin(it) })
+        for (point in points) {
+            pq.add(point)
+        }
+        for(i in 0 until k){
+            ans[i] = pq.poll()
+        }
+        return ans
+    }
+
+    private fun getDistanceFromOrigin(intArray: IntArray): Double {
+        val x = 0 - intArray[0]
+        val y = 0 - intArray[1]
+        
+        return sqrt(x.toDouble() * x.toDouble() + y.toDouble() * y.toDouble())
     }
 }
