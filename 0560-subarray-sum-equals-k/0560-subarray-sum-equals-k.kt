9460928ede9af630e0962a1b0726b8cb60b06494
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2025-06-12 09:52:26 +0530

    Time: 51 ms (22.19%), Space: 57.1 MB (5.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.kt
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.kt
@@ -1,13 +1,27 @@
 class Solution {
+    // fun subarraySum(nums: IntArray, k: Int): Int {
+    //     val map = mutableMapOf<Int,Int>()
+    //     map[0] = 1
+    //     var sum = 0
+    //     var total = 0
+    //     for(i in 0 until nums.size) {
+    //         sum += nums[i]
+    //         total += map.getOrPut(sum -k){0} 
+    //         map[sum] = map.getOrPut(sum){0} + 1
+    //     }
+
+    //     return total
+    // }
     fun subarraySum(nums: IntArray, k: Int): Int {
         val map = mutableMapOf<Int,Int>()
-        map[0] = 1
-        var sum = 0
+        val pre = nums.runningFold(0,Int::plus).toIntArray()
         var total = 0
-        for(i in 0 until nums.size) {
-            sum += nums[i]
-            total += map.getOrPut(sum -k){0} 
-            map[sum] = map.getOrPut(sum){0} + 1
+        for(i in 0 until pre.size) {
+
+            if(map.contains(pre[i] - k)){
+                total += map[pre[i] - k]!!
+            } 
+            map[pre[i]] = map.getOrPut(pre[i]){0} + 1 
         }
 
         return total
