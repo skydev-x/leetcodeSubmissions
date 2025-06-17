2f5b790fd38285351a024d16724ccfbe44485e18
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2025-06-17 13:28:59 +0530

    Time: 259 ms (13.67%) | Memory: 51.1 MB (9.00%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.kt
+++ 567-permutation-in-string/permutation-in-string.kt
@@ -1,29 +1,28 @@
 class Solution {
     fun checkInclusion(s1: String, s2: String): Boolean {
-        if (s1.length > s2.length) return false
-
-        val sign = mutableMapOf<Char, Int>()
-        for (c in s1) {
-            sign[c] = sign.getOrDefault(c, 0) + 1
+        if(s2.length < s1.length) return false
+        val windMap = mutableMapOf<Char,Int>()
+        val pattern =  mutableMapOf<Char,Int>()
+        for(i in 0 until s1.length){
+            pattern[s1[i]] = pattern.getOrPut(s1[i]){0} + 1
+            windMap[s2[i]] = windMap.getOrPut(s2[i]){0} + 1
         }
+        if(pattern.keys.all { it in windMap.keys && pattern[it] == windMap[it] }) return true
 
-        val window = mutableMapOf<Char, Int>()
-        for (i in s2.indices) {
-            val char = s2[i]
-            window[char] = window.getOrDefault(char, 0) + 1
+        var l = 0
+        var r = s1.length - 1
+        while(r < s2.length -1){
+            l++
+            r++
+            if(r > s2.length) break
+            windMap[s2[l-1]] = windMap.getOrPut(s2[l-1]){0} - 1
+            windMap[s2[r]] = windMap.getOrPut(s2[r]){0} + 1
+            println("$pattern and $windMap")
+            if(pattern.keys.all { it in windMap.keys && pattern[it] == windMap[it] }) return true
 
-            if (i >= s1.length) {
-                val leftChar = s2[i - s1.length]
-                if (window[leftChar] == 1) {
-                    window.remove(leftChar)
-                } else {
-                    window[leftChar] = window[leftChar]!! - 1
-                }
-            }
-
-            if (window == sign) return true
         }
 
         return false
+
     }
 }
