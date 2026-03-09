decd1a8a91fc4dde6bd4a04df7916418e50690bb
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2026-03-09 11:33:06 +0530

    N/A (0%) N/A (0%) 03-09-2026 0268-missing-number Easy .kt

---
+++ LeetCode/Easy/0268-missing-number/0268-missing-number-03-09-2026-11-33-04.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+        val seen = BooleanArray(nums.size + 1)
+        for(i in seen.indices){
+            seen[i] = false
+        }
+        for(n in nums){
+            seen[n] = true
+        }
+        for(i in seen.indices){
+           if(seen[i] == false) return i
+        }
+        return -1
+    }
+}
+
+// using set
+    // fun missingNumber(nums: IntArray): Int {
+    //     val set = nums.toSet()
+    //     for(i in 0..nums.size + 1){
+    //         if(i !in set){
+    //             return i
+    //         }
+    //     }
+    //     return -1
+    // }
