c1d8e10b8a24c7e8fa7c37e8eb77b80c57b6814a
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2026-03-10 09:00:25 +0530

    N/A (0%) N/A (0%) 03-10-2026 0102-binary-tree-level-order-traversal Medium .kt

---
+++ LeetCode/Medium/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal-03-10-2026-09-00-25.kt
@@ -0,0 +1,30 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+        val result = mutableListOf<List<Int>>()
+        if (root == null) return result
+        val queue = ArrayDeque<TreeNode>()
+        queue.addLast(root)
+        while (queue.isNotEmpty()) {
+            val level = mutableListOf<Int>()
+            val levelSize = queue.size
+            repeat(levelSize) { 
+                val cur = queue.removeFirst()
+                level.add(cur.`val`)
+                cur.left?.let { queue.addLast(it) }
+                cur.right?.let { queue.addLast(it) }
+            }
+            result.add(level)
+        }
+        return result
+    }
+}
