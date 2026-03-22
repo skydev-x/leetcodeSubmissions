7f1e6286353a3b211a7c11f7fd058adbcab37b63
Sanjeet Yadav <87947328+skydev-x@users.noreply.github.com>
2026-03-22 19:01:42 +0530

    N/A (0%) N/A (0%) 03-22-2026 0050-powx-n Medium .kt

---
+++ LeetCode/Medium/0050-powx-n/0050-powx-n-03-22-2026-19-01-40.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun myPow(x: Double, n: Int): Double {
+        val N = n.toLong()
+        fun pow(x: Double, n: Long) : Double {
+            if(n == 0L) return 1.0
+            val half = pow(x, n/2)
+            return if(n % 2L == 0L) half * half else x * half * half
+        }
+        return if(n >= 0) pow(x,N) else 1/pow(x,-N)
+    }
+}
